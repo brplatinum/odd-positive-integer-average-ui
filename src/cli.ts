e559a7beed008917865ac955cd2f8a7645b13a01
Reece White <reece.white@cloudsense.com>
2021-04-27 19:32:37 +1000

    Finished CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,30 @@
 #!/usr/bin/env npx ts-node
 
+const fetcher = require("node-fetch");
+
 // Main CLI file...
 
-console.log('hello world!');
+let args = process.argv.slice(2);
+
+console.log(args);
+
+let url: string = args[0];
+let numberArray: number[] = JSON.parse(args[1]);
+
+fetcher(url + "odd-positive-average", {
+    method: 'POST',
+    body: JSON.stringify(numberArray),
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+    .then((response) => {
+        if (response.status == 200 || response.status == 400){
+            return response.json();
+        } else {
+            console.log("Didn't recieve either a 200 or 400 status code")
+        }
+    })
+    .then((data) => {
+        console.log(data);
+    })
